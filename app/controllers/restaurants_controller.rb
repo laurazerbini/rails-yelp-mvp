68d40b8c819d0140ee7bb40e67196f7562f8eecd
laura zerbini <lauraczerbini@gmail.com>
2021-04-15 18:14:19 +0200

    yelp mvp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,19 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    #if statement for valid params?
+    # else?
+  end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,12 @@
 class ReviewsController < ApplicationController
+def new
+  @restaurant = Restaurant.find(params[:restaurant_id])
+  @review = Review.new
+end
+
+def create
+  #if statement for valid params?
+  # else?
+end
+
 end
